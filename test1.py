5dbbb0090cdbdb133edfab78409281ec36e514d8
luk <lalaluk5@ntub.edu.tw>
2023-06-28 10:42:14 +0800

    commit 3

--- test1.py
+++ test1.py
@@ -1,4 +1,6 @@
 b = 'cc'
+b2 = 'cc'
+b3 = 'cc'
 
 def test():
     print('test1.py')
--- test2.py
+++ test2.py
@@ -3,3 +3,11 @@ a = 'bb'
 def test():
     print('test1.py')
     print('b =', b)
+
+def test2():
+    print('test1.py')
+    print('b =', b)
+
+def test3():
+    print('test1.py')
+    print('b =', b)
